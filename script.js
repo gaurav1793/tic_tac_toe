4e1f46e41a41ce50bd2ed4fe829af71e464dc7be
Gaurav <katariagaurav686@gmail.com>
2025-03-20 16:59:39 +0530

    added restart button and win logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let div = document.getElementById('outer');
     let chance = false;
+    let arr = Array(9).fill(undefined);
 
     div.addEventListener('click',(e)=>{
         let cell = e.target
@@ -11,9 +12,59 @@ document.addEventListener('DOMContentLoaded',()=>{
         cell.setAttribute('data-clicked','true');
         if(chance){
             cell.textContent='O';
+            arr[cellNumber]='O';
+            check('O')
         }else{
             cell.textContent='X';
+            arr[cellNumber]='X';
+            check('X')
         }
         chance=!chance;
     })
+
+    function check(char){
+        if(arr[0]==char && arr[1]==char && arr[2]==char){
+           restart(char);
+           return;
+        }
+        else if(arr[0]==char && arr[3]==char && arr[6]==char){
+            restart(char);
+        }
+        else if(arr[0]==char && arr[4]==char && arr[8]==char){
+            restart(char);
+        }
+        else if(arr[1]==char && arr[4]==char && arr[7]==char){
+            restart(char);
+        }
+        else if(arr[2]==char && arr[5]==char && arr[8]==char){
+            restart(char);
+        }
+        else if(arr[2]==char && arr[4]==char && arr[6]==char){
+            restart(char);
+        }
+        else if(arr[3]==char && arr[4]==char && arr[5]==char){
+            restart(char);
+        }
+        else if(arr[6]==char && arr[7]==char && arr[8]==char){
+            restart(char);
+        }
+    }
+
+    function restart(char){
+        const finish = document.createElement('div');
+        finish.classList='finish';
+        finish.textContent=`Game Over  ${char} Wins`;
+        div.appendChild(finish);
+        
+
+        const btn = document.createElement('btn');
+        btn.classList='restart';
+        btn.textContent='Restart';
+        btn.style.position='absolute';
+        document.body.appendChild(btn);
+        btn.addEventListener('click',()=>{
+            window.location.reload();
+            return;
+        })
+    }
 })
